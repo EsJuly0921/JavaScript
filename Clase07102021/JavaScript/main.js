330b63a5e3217bd38403366c2c4ac99c64a53b5d
JulioG <julioclg30@outlook.com>
2021-10-07 21:09:55 -0500

    Segunda  clase de JavaScript

---
+++ Clase07102021/JavaScript/main.js
@@ -0,0 +1,28 @@
+/*var xMen = { Objeto <--- xMen["apodo"] o xMen.apodo
+    apodo: 'wolverine',
+    nombre: 'logan',
+    condicion: 'mutante',
+    genero: 'hombre',
+    soltero: true,
+    pasatiempos: [
+        'matar',
+        'garras',
+    ]
+};
+
+var xMen2 = [ <--- Array
+    'wolverine',
+    'logan',
+    'mutante',
+    'hombre',
+    true
+];*/
+
+var card1 = {
+    name: 'luis angel',
+    levels: 10,
+    type: 'Frío/ADA',
+    info: 'Este monstruo de angular se alimenta de JavaScript. Su debilidad son los corridos tumbados.',
+    atk: 1200,
+    def: 900,
+}
