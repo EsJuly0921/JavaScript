b1cfb8bc4428508f9240dc976b46b6f49ad6c801
JulioG <julioclg30@outlook.com>
2021-10-12 21:05:56 -0500

    Actualización del día 12102021

--- Clase07102021/JavaScript/main.js
+++ Clase07102021/JavaScript/main.js
@@ -18,29 +18,156 @@ var xMen2 = [ <--- Array
     true
 ];*/
 
-var card1 = {
-    name: 'luis angel',
-    levels: 10,
-    type: 'Frío/ADA',
-    info: 'Este monstruo de angular se alimenta de JavaScript. Su debilidad son los corridos tumbados.',
-    atk: 1200,
-    def: 900,
+var tiktokers = [
+    {
+        name: 'luis angel',
+        stars: 10,
+        type: 'Frío/ADA',
+        info: 'Este monstruo de angular se alimenta de JavaScript. Su debilidad son los corridos tumbados.',
+        atk: 1200,
+        def: 900,
+    },
+    {
+        name: "KaoLo",
+        stars: 7,
+        type: "Especialista / Normal",
+        info: "Experta en horóscopos, habilidad sobre humana para el baile, debilidad maquetación en HTML",
+        atk: 2500,
+        def: 2200,
+    },
+    {
+        name: 'Cesar García',
+        stars:1,
+        type:'[Demonio / Efecto ]',
+        info:'Alias Cesar Código Bits, habilidad experto en wordpress y en wix, debilidad la chancla, este demonio es hermano de Kuriboh',
+        atk: 300,
+        def: 200 ,
+    },
+    {
+        nombre: 'Gerardo Schekaiban',
+        stars: 5,
+        type: 'Guerrero/Fusion',
+        info: 'Poder especial "Yo soy tu amigo fiel de la programacion" aumenta el poder de tus otros programadores en el campo de batalla, debilidad Andy',
+        atk: 1800,
+        def: 1200,
+    },
+    {
+        name:'Edrissi Machorro',
+        stars:7,
+        type:'Dragon/Fusión',
+        info:'Habilidad dormir, y hacerle broma a Cesar Código Bits, cuando está carta en juego junto con la de Cesar García, Cesar García pierde 100 ATK',
+        atk:2400,
+        def:200,
+    },
+    {
+        name: 'francisco jiménez',
+        stars: 2,
+        type: 'Lanzador de conjuros / Efecto',
+        info: 'Alias Pamsho.dev hsbilidad maestro en las artes místicas de JavaScript y sacar tiempo para redes sociales, stream, trabajar y estudiar pero además tener pareja, debilidad la vacuna del covid',
+        atk: 500,
+        def: 400,
+    },
+    {
+        name: 'toledo floang',
+        stars: 12,
+        type: 'Bestía Divina/EFECTO',
+        info: 'Alias: Ingesaurio. Habilidad especial: Dañar libros de ingeniería y dar consejos de ingenieria. En cada ronda aumenta 500ATK.',
+        atk: 4000,
+        def: 4000,
+    },
+    {
+        name: 'stefan castillo',
+        stars: 10,
+        type: 'Hechicero/EFECTO',
+        info: 'Habilidad para las artes oscuras del CSS mágico. Habilidad especial: Logos con CSS. Debilidad: Usar JavaScript le quita 100 ATK',
+        atk: 2200,
+        def: 2000,
+    },
+    {
+        name: "Maeva Mazadiego",
+        stars: 12,
+        type: "Hechicero / Efecto",
+        info: "Alias Mae, habilidad seriedad absoluta divina, cuando se encuentra en el juego Chica Python Maeva obtiene 1000 ATK",
+        atk: 3200,
+        def: 3000,
+    },
+    {
+        name: 'jessica azcona',
+        stars: 9,
+        type: 'hechicera',
+        info: 'Hablilidad magia antigua que es capaz de borrar cuentas de github, debilidad C, C++',
+        atk: 1200,
+        def: 900,
+    },
+    {
+        name: 'Rodo Ferro',
+        stars:12,
+        type:'[ Mona China / Synchro / Effect ]',
+        info:'Habilidad sobre humana para generación de monas chinas para atacar con su ejército, si en el campo de batalla se encuentra Carlita.py evangelizan el doble de rápido al mundo del IA, debilidad el hermano de Kuriboh',
+        atk:3000,
+        def:2500,
+    },
+    {
+        name: 'joss pelcastre',
+        stars: 8,
+        type: 'Metal/Efect',
+        info: 'Habilidad divina de charla astral, especial hackear Facebook de tu ex, si se encuentra al hermano de Kuriboh se igualan sus ATK y DEF',
+        atk: 1900,
+        def: 1400,
+    },
+    {
+        name: 'david portilla',
+        stars: 8,
+        type: 'Mona China / Metal / Efect',
+        info: 'Alias el Frontend Mecatronico Habilidad especial jugar pokimon con el Rodo Ferro y sus monas chinas, si en el campo se encuentra a Pamsho.dev incrementa ambos su poder con esteroides a 2500 ATK y DEF',
+        atk: 1900,
+        def: 1900,
+    },
+]
+
+var main = document.getElementById('app');
+
+for (let index = 0; index < tiktokers.length; index++) {
+
+    var element = document.createElement('div');
+    element.classList.add('card');
+    element.innerHTML = ` <div class="cardMain">
+<div class="titulo">
+    <span>${tiktokers[index].name}</span>
+</div>
+<div class="stars">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+    <img src="../Clase07102021/img/Cards/star.png" alt="">
+</div>
+<div class="img">
+    <div></div>
+</div>
+<div class="info">
+    <div>
+        <p class="type">
+            [FRÍO/ADA  ]
+        </p>
+        <p class="description">
+            ${tiktokers[index].info}
+        </p>
+        <p class="num">
+            ATK/1200 DEF/900
+        </p>
+    </div>
+</div>
+</div>`;
+
+main.appendChild(element);
+
 }
 
-var card12 = {
-    name: 'toledo floang',
-    levels: 12,
-    type: 'Bestía Divina/EFECTO',
-    info: 'Alias: Ingesaurio. Habilidad especial: Dañar libros de ingeniería y dar consejos de ingenieria. En cada ronda aumenta 500ATK.',
-    atk: 4000,
-    def: 4000,
-}
-
-var card13 = {
-    name: 'stefan castillo',
-    levels: 10,
-    type: 'Hechicero/EFECTO',
-    info: 'Hbailidad para las artes oscuras del CSS mágico. Habilidad especial: Logos con CSS. Debilidad: Usar JavaScript le quita 100 ATK',
-    atk: 2200,
-    def: 2000,
-}
